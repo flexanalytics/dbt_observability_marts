b09ab24721a80bfac96f6b5db4fda3a17f9235fd
Michael Connell <connellmp@gmail.com>
2024-08-28 19:00:54 -0700

    properly split source node id

--- models/intermediate/int_source.sql
+++ models/intermediate/int_source.sql
@@ -5,10 +5,10 @@
 }}
 select
     command_invocation_id,
-    node_id,
+    node_id,  -- resource_type.project.source_definition_name.table
     {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=1) }} as resource_type,
     {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=2) }} as project,
-    {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=3) }} as resource_name,
+    {{ dbt.split_part(string_text='node_id', delimiter_text="'.'", part_number=4) }} as resource_name,
     run_started_at,
     database_name,
     schema_name,
