3c1cce55c3d7a1ff26a4264833486645ac51e3ab
Andrew Taft <11670864+ataft@users.noreply.github.com>
2024-10-03 11:12:52 -0700

    Execution Dimension Improvements
    * Simplify logic to use rank() window function
    * Added attributes: run_started_at, was_full_refresh, thread_id, compile_started_at, query_completed_at
    * Added "node_rank" and "Most Recent Node Run"
    * Added "node_rank_per_day" and "Most Recent Node Run Per Day"
    * Leaving most_recent_run for now, deprecating and moving to dim_invocation.most_recent_run in the future

--- models/marts/dim_execution.sql
+++ models/marts/dim_execution.sql
@@ -14,23 +14,45 @@ with
             status,
             materialization,
             schema_name,
-            run_started_at
-        from {{ ref('int_execution') }}
-    ),
+            run_started_at,
+            was_full_refresh,
+            thread_id,
+            compile_started_at,
+            query_completed_at,
+            rank() over (order by run_started_at desc) as run_rank,
+            rank() over (partition by node_id order by run_started_at desc) as node_rank,
+            rank() over (partition by node_id, cast(run_started_at as date) order by run_started_at desc) as node_rank_per_day
 
-    most_recent as (
-        select max(run_started_at) as most_recent_run
-        from executions
+        from {{ ref('int_execution') }}
     )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['executions.command_invocation_id', 'executions.node_id']) }} as execution_key,
-    executions.node_id,
-    executions.resource_type,
-    executions.project,
-    executions.resource_name,
-    executions.status,
-    executions.materialization,
-    executions.schema_name,
-    case when executions.run_started_at = most_recent.most_recent_run then 'Yes' else 'No' end as most_recent_run
-from executions left join most_recent on executions.run_started_at = most_recent.most_recent_run
+    {{ dbt_utils.generate_surrogate_key(['command_invocation_id', 'node_id']) }} as execution_key,
+    run_started_at,
+    compile_started_at,
+    query_completed_at,
+    was_full_refresh,
+    thread_id,
+    node_id,
+    resource_type,
+    project,
+    resource_name,
+    status,
+    materialization,
+    schema_name,
+    run_rank,
+    node_rank,
+    node_rank_per_day,
+    case
+        when run_rank = 1 then 'Yes'
+        else 'No'
+    end as most_recent_run,
+    case
+        when node_rank = 1 then 'Yes'
+        else 'No'
+    end as most_recent_node_run,
+    case
+        when node_rank_per_day = 1 then 'Yes'
+        else 'No'
+    end as most_recent_node_run_per_day
+from executions
